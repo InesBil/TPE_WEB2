8b09f48934842cf558d0f95169d7a7b6d1995b86
Maria Ines Bilbao <mainesbilbao@gmail.com>
2022-10-13 17:14:56 -0300

    Home funcionando

--- router.php
+++ router.php
@@ -1,16 +1,24 @@
 <?php
 require_once './app/controllers/discography.controller.php';
+require_once './app/controllers/record.controller.php';
+require_once './app/controllers/home.controller.php';
 
 define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
 
-$action = 'home'; 
+
 if (!empty($_GET['action'])) {
     $action = $_GET['action'];
+} else {
+  $action = 'home'; 
 }
 $params = explode('/', $action);
 
 // tabla de ruteo
 switch ($params[0]) {
+    case 'home':
+      $homeController = new HomeController();
+      $homeController-> showHome();
+      break;
     case 'showDiscography':
       $discographyController = new DiscographyController();
        $discographyController->showDiscography();
